2710af7a402ea2c3f00677dfa413583fd5911c41
Maciej Sugajski <Maciek@MacBook-Air-Maciej.local>
2017-05-13 20:36:27 +0200

    Initial Commit

---
+++ Koleo-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Koleo-iOS
+//
+//  Created by Maciej Sugajski on 13.05.2017.
+//  Copyright © 2017 sugajski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
